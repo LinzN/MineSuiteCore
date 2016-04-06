ec491a5128f5bc937712c5de6a3f444d952632b6
Niklas PC <Niklas@DESKTOP-DFRD0P4>
2016-04-06 12:32:32 +0200

    Add Build number

--- src/main/java/de/kekshaus/cookieApi/bukkit/commands/CApiCommand.java
+++ src/main/java/de/kekshaus/cookieApi/bukkit/commands/CApiCommand.java
@@ -4,26 +4,65 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 
-import de.kekshaus.cookieApi.bukkit.GlobalMessageDB;
+import net.md_5.bungee.api.ChatColor;
 
 public class CApiCommand implements CommandExecutor {
 	public ThreadPoolExecutor executorServiceCommands = new ThreadPoolExecutor(1, 1, 250L, TimeUnit.MILLISECONDS,
 			new LinkedBlockingQueue<Runnable>());
 
 	public boolean onCommand(final CommandSender sender, Command cmnd, String label, final String[] args) {
-		if (sender.hasPermission("cookieApi.api.command")) {
-			this.executorServiceCommands.submit(new Runnable() {
-				public void run() {
-					sender.sendMessage("CookieApi Helpseite:");
+		this.executorServiceCommands.submit(new Runnable() {
+			public void run() {
+				Plugin bukkit = Bukkit.getPluginManager().getPlugin("CookieApiBukkit");
+				Plugin ban = Bukkit.getPluginManager().getPlugin("CookieApiBan");
+				Plugin chat = Bukkit.getPluginManager().getPlugin("CookieApiChat");
+				Plugin guild = Bukkit.getPluginManager().getPlugin("CookieApiGuild");
+				Plugin home = Bukkit.getPluginManager().getPlugin("CookieApiHome");
+				Plugin portal = Bukkit.getPluginManager().getPlugin("CookieApiPortal");
+				Plugin teleport = Bukkit.getPluginManager().getPlugin("CookieApiTeleport");
+				Plugin warp = Bukkit.getPluginManager().getPlugin("CookieApiWarp");
+				sender.sendMessage(ChatColor.DARK_PURPLE + "CookieApi Version:");
+				if (bukkit != null) {
+					sender.sendMessage(ChatColor.DARK_GREEN + "CookieApiBukkit: " + ChatColor.DARK_PURPLE
+							+ ChatColor.BOLD + bukkit.getDescription().getVersion());
 				}
-			});
-		} else {
-			sender.sendMessage(GlobalMessageDB.NO_PERMISSIONS);
-		}
+				if (ban != null) {
+					sender.sendMessage(ChatColor.DARK_GREEN + "CookieApiBan: " + ChatColor.DARK_PURPLE + ChatColor.BOLD
+							+ ban.getDescription().getVersion());
+				}
+				if (chat != null) {
+					sender.sendMessage(ChatColor.DARK_GREEN + "CookieApiChat: " + ChatColor.DARK_PURPLE + ChatColor.BOLD
+							+ chat.getDescription().getVersion());
+				}
+				if (guild != null) {
+					sender.sendMessage(ChatColor.DARK_GREEN + "CookieApiGuild: " + ChatColor.DARK_PURPLE
+							+ ChatColor.BOLD + guild.getDescription().getVersion());
+				}
+				if (home != null) {
+					sender.sendMessage(ChatColor.DARK_GREEN + "CookieApiHome: " + ChatColor.DARK_PURPLE + ChatColor.BOLD
+							+ home.getDescription().getVersion());
+				}
+				if (portal != null) {
+					sender.sendMessage(ChatColor.DARK_GREEN + "CookieApiPortal: " + ChatColor.DARK_PURPLE
+							+ ChatColor.BOLD + portal.getDescription().getVersion());
+				}
+				if (teleport != null) {
+					sender.sendMessage(ChatColor.DARK_GREEN + "CookieApiTeleport: " + ChatColor.DARK_PURPLE
+							+ ChatColor.BOLD + teleport.getDescription().getVersion());
+				}
+				if (warp != null) {
+					sender.sendMessage(ChatColor.DARK_GREEN + "CookieApiWarp: " + ChatColor.DARK_PURPLE + ChatColor.BOLD
+							+ warp.getDescription().getVersion());
+				}
+			}
+		});
+
 		return false;
 	}
 }
