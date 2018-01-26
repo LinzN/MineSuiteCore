0a5762b77f66ccf5fcdb578a360ba03cb37de129
Niklas <niklas.linz@hotmail.com>
2018-01-26 17:54:16 +0100

    Update uuid use

---
+++ src/main/java/de/linzn/mineSuite/core/database/hashDatabase/PendingTeleportsData.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2018. MineGaming - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the LGPLv3 license, which unfortunately won't be
+ * written for another century.
+ *
+ *  You should have received a copy of the LGPLv3 license with
+ *  this file. If not, please write to: niklas.linz@enigmar.de
+ *
+ */
+
+package de.linzn.mineSuite.core.database.hashDatabase;
+
+import org.bukkit.Location;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.UUID;
+
+public class PendingTeleportsData {
+
+    public static HashMap<UUID, Location> pendingLocations = new HashMap<>();
+    public static HashSet<UUID> ignoreActions = new HashSet<>();
+    public static HashMap<UUID, Location> checkMoveLocation = new HashMap<>();
+
+
+    public static void cleanPlayer(UUID playerUUID) {
+        pendingLocations.remove(playerUUID);
+        ignoreActions.remove(playerUUID);
+        checkMoveLocation.remove(playerUUID);
+    }
+
+}
--- src/main/java/de/linzn/mineSuite/core/database/mysql/MySQLConnectionSetup.java
+++ src/main/java/de/linzn/mineSuite/core/database/mysql/MySQLConnectionSetup.java
@@ -39,24 +39,9 @@ public class MySQLConnectionSetup {
 
         try {
             Connection connection = mineSuiteCoreHandler.getConnection();
-            String warp = "CREATE TABLE IF NOT EXISTS warps (player VARCHAR(100), warp_name VARCHAR(100), server VARCHAR(100), world text, x double, y double, z double, yaw float, pitch float, visible int, PRIMARY KEY (`warp_name`));";
-            String teleport = "CREATE TABLE IF NOT EXISTS spawns (Id int NOT NULL AUTO_INCREMENT, spawntype VARCHAR(100), server VARCHAR(100), world text, x double, y double, z double, yaw float, pitch float, visible int, PRIMARY KEY (Id));";
-            String portal = "CREATE TABLE IF NOT EXISTS portals (portalname VARCHAR(100), server VARCHAR(100), type VARCHAR(20), destination VARCHAR(100), world VARCHAR(100), filltype VARCHAR(100) DEFAULT 'AIR', xmax INT(11), xmin INT(11), ymax INT(11), ymin INT(11), zmax INT(11), zmin INT(11), CONSTRAINT pk_portalname PRIMARY KEY (portalname));";
-            String home = "CREATE TABLE IF NOT EXISTS homes (player VARCHAR(100), home_name VARCHAR(100), server VARCHAR(100), world text, x double, y double, z double, yaw float, pitch float, PRIMARY KEY (`player`,`home_name`,`server`));";
-            String guild1 = "CREATE TABLE IF NOT EXISTS guilds (GuildUUID VARCHAR(100), GuildName VARCHAR(100), Level int, Experience bigint, PRIMARY KEY (GuildName));";
-            String guild2 = "CREATE TABLE IF NOT EXISTS guildSpawns (GuildUUID VARCHAR(100), Server text, World text, CordX double, CordY double, CordZ double, Yaw float, Pitch float, PRIMARY KEY (GuildUUID));";
-            String guild3 = "CREATE TABLE IF NOT EXISTS guildPlayers (PlayerName VARCHAR(100), UUID text, GuildUUID text, GuildRang text, PRIMARY KEY (PlayerName));";
             Statement action = connection.createStatement();
-            action.executeUpdate(warp);
-            action.executeUpdate(teleport);
-            action.executeUpdate(portal);
-            action.executeUpdate(home);
-            action.executeUpdate(guild1);
-            action.executeUpdate(guild2);
-            action.executeUpdate(guild3);
             action.close();
             mineSuiteCoreHandler.release(connection);
-
             return true;
 
         } catch (Exception e) {
