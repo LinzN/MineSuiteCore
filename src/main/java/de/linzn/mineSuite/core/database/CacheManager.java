0ea553307780e296568f436fd79aa4dbdce46e66
Niklas <niklas.linz@hotmail.com>
2019-02-24 19:45:06 +0100

    Update cachemanager

--- src/main/java/de/linzn/mineSuite/core/database/CacheManager.java
+++ src/main/java/de/linzn/mineSuite/core/database/CacheManager.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 public class CacheManager {
     private static Map<UUID, String> playerCache = new HashMap<>();
 
-    public static UUID getPlayerUUID(String playerName) {
+    public static UUID getPlayerUUID(String playerName, boolean localOnly) {
         if (playerCache.containsValue(playerName)) {
             for (UUID uuid : playerCache.keySet()) {
                 if (playerCache.get(uuid).equalsIgnoreCase(playerName)) {
@@ -25,12 +25,14 @@ public class CacheManager {
             playerCache.put(playerUUID, playerName);
             return playerUUID;
         } else {
-            playerUUID = new RequestMojangAPI().fetchUUID(playerName);
+            if(!localOnly) {
+                playerUUID = new RequestMojangAPI().fetchUUID(playerName);
+            }
         }
 
-        if (playerUUID == null) {
-            playerUUID = Bukkit.getOfflinePlayer(playerName).getUniqueId();
-        }
+        //if (playerUUID == null) {
+        //    playerUUID = Bukkit.getOfflinePlayer(playerName).getUniqueId();
+        //}
 
         if (playerUUID != null) {
             playerCache.put(playerUUID, playerName);
@@ -38,30 +40,31 @@ public class CacheManager {
         return playerUUID;
     }
 
-    public UUID[] getPlayerUUIDs(String[] playerNames) {
+    public UUID[] getPlayerUUIDs(String[] playerNames, boolean localOnly) {
         UUID[] playerUUIDS = new UUID[playerNames.length];
         for (int i = 0; i < playerNames.length; i++) {
-            playerNames[i] = getPlayerName(playerUUIDS[i]);
+            playerNames[i] = getPlayerName(playerUUIDS[i], localOnly);
         }
         return playerUUIDS;
     }
 
-    public static String getPlayerName(UUID playerUUID) {
+    public static String getPlayerName(UUID playerUUID, boolean localOnly) {
         if (playerCache.containsKey(playerUUID)) {
             return playerCache.get(playerUUID);
         }
         String playerName = BukkitQuery.getPlayerName(playerUUID);
-
         if (playerName != null) {
             playerCache.put(playerUUID, playerName);
             return playerName;
         } else {
-            playerName = new RequestMojangAPI().fetchName(playerUUID);
+            if(!localOnly) {
+                playerName = new RequestMojangAPI().fetchName(playerUUID);
+            }
         }
 
-        if (playerName == null) {
-            playerName = Bukkit.getOfflinePlayer(playerUUID).getName();
-        }
+        //if (playerName == null) {
+        //    playerName = Bukkit.getOfflinePlayer(playerUUID).getName();
+        //}+
 
         if (playerName != null) {
             playerCache.put(playerUUID, playerName);
@@ -69,10 +72,10 @@ public class CacheManager {
         return playerName;
     }
 
-    public String[] getPlayerNames(UUID[] playerUUIDs) {
+    public String[] getPlayerNames(UUID[] playerUUIDs, boolean localOnly) {
         String[] playerNames = new String[playerUUIDs.length];
         for (int i = 0; i < playerNames.length; i++) {
-            playerNames[i] = getPlayerName(playerUUIDs[i]);
+            playerNames[i] = getPlayerName(playerUUIDs[i], localOnly);
         }
         return playerNames;
     }
